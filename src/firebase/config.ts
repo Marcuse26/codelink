c36070eb916ae93a544dbac6c4fd96c42d9f6889
Marcuse26 <marcos.serranomartinez.alumnos@gmail.com>
2025-11-30 23:58:59 +0100

    fix: Agregadas claves reales de Firebase y verificado CSS

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,14 +3,15 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // SUSTITUYE ESTO CON LOS DATOS QUE COPIASTE DE LA CONSOLA DE FIREBASE
-  apiKey: "AIzaSy...",
-  authDomain: "codelink-....firebaseapp.com",
-  projectId: "codelink-...",
-  storageBucket: "codelink-....appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyDkG4V4zKRMNHcpaeWRNDIFjwhXgP9YO8Q",
+  authDomain: "codelink-af067.firebaseapp.com",
+  projectId: "codelink-af067",
+  storageBucket: "codelink-af067.firebasestorage.app",
+  messagingSenderId: "147375276218",
+  appId: "1:147375276218:web:d6040312600790347b8283",
+  measurementId: "G-1NDKVXS63B"
 };
+
 
 // Inicializar Firebase solo si no existe ya
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
